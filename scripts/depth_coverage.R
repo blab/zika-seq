3bffe1624e80c6713fe4e754b293788c4b3a8c80
Barney Potter <barney.potter.24@gmail.com>
2016-12-30 13:52:42 -0800

    Clean makeOverlapGraphs

--- scripts/depth_coverage.R
+++ scripts/depth_coverage.R
@@ -13,6 +13,7 @@
 #         NB04,NB10
 #         NB05,NB11
 #         NB06,NB12
+
 roundUpNice <- function(x, nice=c(1,2,4,5,6,8,10)) {
   if(length(x) != 1) stop("'x' must be of length 1")
   10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
