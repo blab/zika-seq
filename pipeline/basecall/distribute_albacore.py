9838cae29540dd2c8d0b8fc35ee761ba94080da3
Barney Potter <barney.potter.24@gmail.com>
2017-05-01 16:45:10 -0700

    Add albacore distribution and file cleaning for slurm.

--- pipeline/basecall/distribute_albacore.py
+++ pipeline/basecall/distribute_albacore.py
@@ -5,10 +5,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--inpath', default=None, help="path to input directory containing non-basecalled reads")
 parser.add_argument('-o', '--outpath', default=None, help="path to output directory for basecalled reads")
-parser.add_argument('--test', default=False, action='store_true', help="make sure that albacore is working correctly on a subset of the data")
-parser.add_argument('--testdir', default=None, help="directory for debugging")
 parser.add_argument('--dimension', default='1d', help="dimension of sequenced library; 1d or 2d")
 parser.add_argument('--email', default=None, help="email address for sbatch notifications")
+
+parser.add_argument('--test', default=False, action='store_true', help="make sure that albacore is working correctly; do not run from SBATCH")
 parser.add_argument('--dryrun', default=False, action='store_true', help="print commands to be run, but do not call them")
 args = parser.parse_args()
 
@@ -18,22 +18,21 @@ else:
     w = 'r94_250bps_2d.cfg'
 
 if args.test:
-    print('######################')
-    assert args.testdir, "Unable to test without test directory"
-    i = args.testdir + 'in/'
-    o = args.testdir + 'out/'
-    for dirname in os.listdir(i):
-        call = 'read_fast5_basecaller.py -i %s%s/ -t 1 -c %s -r --barcoding -s %s'%(i, dirname, w, o)
-        print(call)
-        if not args.dryrun:
-            os.system(call)
+    assert args.email, "No email address given"
+    call = 'sbatch --wrap=\"read_fast5_basecaller.py -h\"'
+    print(call)
+    try:
+        subprocess.call(call, shell=True)
+    except:
+        print("Unable to run Albacore. Ensure that proper requisite module is loaded with:\nmodule load Python/3.5.2-foss-2016b-fh1")
 else:
     assert args.inpath, "No input path given"
     assert args.outpath, "No output path given"
-    assert args.email, "No email address given"
+    assert args.email, "No email address given; necessary for SBATCH"
+
     for dirname in os.listdir(args.inpath):
-        acall = 'read_fast5_basecaller.py -i %s%s/ -t 6 -c %s -r --barcoding -s %s'%(args.inpath, dirname, w, args.outpath)
-        call = [ 'sbatch', '--time=48:00:00', '--mem=10000', '--mail-type=END,FAIL', '--mail-user=%s'%(args.email), '--wrap=\"%s\"'%(acall) ]
+        acall = 'read_fast5_basecaller.py -i %s/%s/ -t 6 -c %s -r --barcoding -s %s'%(args.inpath, dirname, w, args.outpath)
+        call = [ 'sbatch', '--time=24:00:00', '--mem=10000', '--mail-type=FAIL', '--mail-user=%s'%(args.email), '--wrap="%s"'%(acall) ]
         print(" ".join(call))
         if not args.dryrun:
-            subprocess.call(call)
+            subprocess.call(" ".join(call), shell=True)
---
+++ pipeline/basecall/remove_tmp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse, os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', default='/fh/fast/bedford_t/zika-seq/data/usvi-library8-1d-2017-03-31/tmp/tmp/')
+parser.add_argument('-o', '--output', default='/fh/fast/bedford_t/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/')
+
+def rename_contents(inpath, outpath):
+    for old_fname in os.listdir(inpath):
+        if old_fname[-4:] == '.tmp':
+            new_fname = old_fname[:-4]
+            old_path = inpath + old_fname
+            new_path = outpath + new_fname
+            try:
+                os.rename(old_path, new_path)
+            except:
+                pass
+
+def make_directory(path, folder):
+    directory = path + folder
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+if __name__=="__main__":
+    args = parser.parse_args()
+
+    for folder in os.listdir(args.input):
+        make_directory(args.output, folder)
+        infolder = args.input + folder + '/'
+        outfolder = args.output + folder + '/'
+        rename_contents(infolder, outfolder)
