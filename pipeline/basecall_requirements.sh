b0e26b68be3e1c530d3e860c14c7b62a7bdee91e
Barney Potter <barney.potter.24@gmail.com>
2017-05-08 22:05:36 -0700

    Add scripts for loading modules for pipeline and basecall.

---
+++ pipeline/basecall_requirements.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+module load Python/3.5.2-foss-2016b-fh1
---
+++ pipeline/pipeline_requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+module load Python/3.6.1-foss-2016b-fh1
+module load R/3.4.0-foss-2016-fh1
+module load BWA/0.7.15-foss-2016b
+module load SAMtools/1.3.1-foss-2016b
+module load nanopolish
