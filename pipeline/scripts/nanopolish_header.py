eadef270945f68c9186f868dbd8646b764e8e6a9
Barney Potter <barney.potter.24@gmail.com>
2017-05-09 15:23:38 -0700

    Update print statements

--- pipeline/scripts/nanopolish_header.py
+++ pipeline/scripts/nanopolish_header.py
@@ -3,9 +3,11 @@
 from Bio import SeqIO
 import sys
 
+print('sys.argv[0]')
+
 recs = list(SeqIO.parse(open(sys.argv[1], "r"), "fasta"))
 if len (recs) != 1:
     print >>sys.stderr, "FASTA has more than one sequence"
     raise SystemExit
 
-print "%s:%d-%d" % (recs[0].id, 1, len(recs[0])+1)
+print("%s:%d-%d" % (recs[0].id, 1, len(recs[0])+1))
