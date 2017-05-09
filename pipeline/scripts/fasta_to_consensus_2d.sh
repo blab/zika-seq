0653d8633e3f3759b1ffb60a2228763c1c64b1b4
Barney Potter <barney.potter.24@gmail.com>
2017-05-08 18:27:13 -0700

    Update nanopolish calls.

--- pipeline/scripts/fasta_to_consensus_2d.sh
+++ pipeline/scripts/fasta_to_consensus_2d.sh
@@ -28,12 +28,14 @@ samtools index $sample.primertrimmed.sorted.bam
 #covplot.R $sample.alignreport.txt
 
 # 5) align the nanopore raw signal against the reference using the previous alignments as a guide
-$EBROOTNANOPOLISH/nanopolish eventalign -vvvv -t 16 --models-fofn new_models.fofn --reads $sample.fasta -b $sample.trimmed.sorted.bam -g $ref --sam | samtools view -bS - | samtools sort -T $sample.tmp - -o $sample.np.sorted.bam
-samtools index $sample.np.sorted.bam
-
-# 6) do variant calling using the raw signal alignment
-$EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`/fh/fast/bedford_t/zika-seq/pipeline/scripts/nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
-$EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`/fh/fast/bedford_t/zika-seq/pipeline/scripts/nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
+# $EBROOTNANOPOLISH/nanopolish eventalign -vvvv -t 16 --models-fofn new_models.fofn --reads $sample.fasta -b $sample.trimmed.sorted.bam -g $ref --sam | samtools view -bS - | samtools sort -T $sample.tmp - -o $sample.np.sorted.bam
+# samtools index $sample.np.sorted.bam
+#
+# # 6) do variant calling using the raw signal alignment
+# $EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`/fh/fast/bedford_t/zika-seq/pipeline/scripts/nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
+# $EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`/fh/fast/bedford_t/zika-seq/pipeline/scripts/nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
+$EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -g $ref -vv -w "`scripts/nanopolish_header.py $ref`" --snps --ploidy 1
+$EBROOTNANOPOLISH/nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -g $ref -vv -w "`scripts/nanopolish_header.py $ref`" --snps --ploidy 1
 
 # 7) filter the variants and produce a consensus
 margin_cons.py $ref $sample.vcf $sample.trimmed.sorted.bam a > $sample.consensus.fasta
