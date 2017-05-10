c7f7715d8df2d87b39a7c537504d17ffb929c333
Barney Potter <barney.potter.24@gmail.com>
2017-05-10 14:58:22 -0700

    Fix string encoding.

--- pipeline/scripts/margin_cons.py
+++ pipeline/scripts/margin_cons.py
@@ -27,9 +27,9 @@ def collect_depths(bamfile):
                              stdout=subprocess.PIPE)
 	out, err = p.communicate()
 	depths = defaultdict(dict)
-	for ln in out.split("\n"):
+	for ln in out.split(b"\n"):
 		if ln:
-			contig, pos, depth = ln.split("\t")
+			contig, pos, depth = ln.split(b"\t")
 			depths[contig][int(pos)] = int(depth)
 	return depths
 
