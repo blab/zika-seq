f5958d143ff34b1e894d4d1d10ff5112f40bcd44
Barney Potter <barney.potter.24@gmail.com>
2017-05-11 16:31:11 -0700

    Add library prep and metrics.

---
+++ pipeline/basecall/count_ratios.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+
+out_file = '/fh/fast/bedford_t/zika-seq/pipeline/basecall/ratios.txt'
+
+prefix = '/fh/fast/bedford_t/zika-seq/data/'
+
+barcodes = [ 'barcode01', 'barcode02', 'barcode03', 'barcode04',
+             'barcode05', 'barcode06', 'barcode07', 'barcode08',
+             'barcode09', 'barcode10', 'barcode11', 'barcode12' ]
+
+libraries = { '1' : 'usvi-library1-2016-12-10',
+              '3' : 'usvi-library3-2017-02-02',
+              '4' : 'usvi-library4-2017-03-03',
+              '5' : 'usvi-library5-2017-03-14',
+              '6' : 'usvi-library6-2017-03-22',
+              '7' : 'usvi-library7-1d-2017-03-24',
+              '8' : 'usvi-library8-1d-2017-03-31' }
+
+with open(out_file, 'w+') as f:
+    f.write('Library\tBasecalled\tUnclassified\tRatio\n')
+    for library in libraries.keys():
+        barcode_count = float(sum( [ len(os.listdir('%s%s/%s/'%(prefix, libraries[library], barcode))) for barcode in barcodes ] ))
+        unclassified_count = float(len(os.listdir('%s%s/unclassified'%(prefix, libraries[library]))))
+        ratio = barcode_count / unclassified_count
+        f.write('%s\t%s\t%s\t%s'%(library, str(barcode_count), str(unclassified_count), str(ratio)))
---
+++ pipeline/basecall/prep_lib.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--library', type = str, help="Library to prep")
+args = parser.parse_args()
+
+libraries = { '3' : 'usvi-library3-2017-02-02',
+              '4' : 'usvi-library4-2017-03-03',
+              '5' : 'usvi-library5-2017-03-14',
+              '6' : 'usvi-library6-2017-03-22',
+              '8' : 'usvi-library8-1d-2017-03-31' }
+
+assert args.library in libraries.keys(), "%s not a valid library"%(args.library)
+
+b = '/fh/fast/bedford_t/zika-seq/data/%s/basecalled_reads/workspace'%(libraries[args.library])
+for bc in os.listdir(b):
+    bcd = '%s/%s'%(b, bc)
+    if bcd[-6:] != '.fast5':
+        for f in os.listdir('%s/0/'%(bcd)):
+            of = '%s/0/%s'%(bcd, f)
+            nf = '%s/%s'%(bcd, f)
+            print('mv %s %s'%(of, nf))
+            os.rename(of, nf)
