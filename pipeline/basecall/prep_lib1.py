9a108303cdd4c12e8cf6a23d0444c7daee34882c
Barney Potter <barney.potter.24@gmail.com>
2017-05-12 14:10:06 -0700

    Check for directory.

--- pipeline/basecall/prep_lib1.py
+++ pipeline/basecall/prep_lib1.py
@@ -4,8 +4,9 @@ import os
 b = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace'
 for bc in os.listdir(b):
     bcd = '%s/%s'%(b, bc)
-    for f in os.listdir('%s/0/'%(bcd)):
-        of = '%s/0/%s'%(bcd, f)
-        nf = '%s/%s'%(bcd, f)
-        print('mv %s %s'%(of, nf))
-        os.rename(of, nf)
+    if os.path.isdir(bcd):
+        for f in os.listdir('%s/0/'%(bcd)):
+            of = '%s/0/%s'%(bcd, f)
+            nf = '%s/%s'%(bcd, f)
+            print('mv %s %s'%(of, nf))
+            os.rename(of, nf)
