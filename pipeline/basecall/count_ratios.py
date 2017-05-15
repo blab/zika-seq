946b6e0e6c1a9d9350b3b46540d403da7a1f3d0f
Alli Black <black.alli@gmail.com>
2017-05-15 13:40:26 -0700

    Fix file paths to route to basecalled reads.

--- pipeline/basecall/count_ratios.py
+++ pipeline/basecall/count_ratios.py
@@ -9,13 +9,13 @@ barcodes = [ 'barcode01', 'barcode02', 'barcode03', 'barcode04',
              'barcode05', 'barcode06', 'barcode07', 'barcode08',
              'barcode09', 'barcode10', 'barcode11', 'barcode12' ]
 
-libraries = { '1' : 'usvi-library1-2016-12-10',
-              '3' : 'usvi-library3-2017-02-02',
-              '4' : 'usvi-library4-2017-03-03',
-              '5' : 'usvi-library5-2017-03-14',
-              '6' : 'usvi-library6-2017-03-22',
-              '7' : 'usvi-library7-1d-2017-03-24',
-              '8' : 'usvi-library8-1d-2017-03-31' }
+libraries = { #'1' : 'usvi-library1-2016-12-10/pass', #remove lib1 until directory structure reformatted
+              '3' : 'usvi-library3-2017-02-02/basecalled_reads/workspace',
+              '4' : 'usvi-library4-2017-03-03/basecalled_reads/workspace',
+              '5' : 'usvi-library5-2017-03-14/basecalled_reads/workspace',
+              '6' : 'usvi-library6-2017-03-22/basecalled_reads/workspace',
+              '7' : 'usvi-library7-1d-2017-03-24/basecalled_reads/workspace',
+              '8' : 'usvi-library8-1d-2017-03-31/basecalled_reads/workspace' }
 
 with open(out_file, 'w+') as f:
     f.write('Library\tBasecalled\tUnclassified\tRatio\n')
