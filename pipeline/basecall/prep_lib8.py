04306a9a56454d21ca532e4258a747886d82c648
Barney Potter <barney.potter.24@gmail.com>
2017-05-15 13:00:42 -0700

    Prep library 8

---
+++ pipeline/basecall/prep_lib8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+
+b = '/fh/fast/bedford_t/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads/workspace'
+for bc in os.listdir(b):
+    bcd = '%s/%s'%(b, bc)
+    for f in os.listdir('%s/0/'%(bcd)):
+        of = '%s/0/%s'%(bcd, f)
+        nf = '%s/%s'%(bcd, f)
+        print('mv %s %s'%(of, nf))
+        os.rename(of, nf)
