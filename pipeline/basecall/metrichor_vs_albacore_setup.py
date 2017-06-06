dfbb321f07fc550b0cc7abecee480f23f3185952
Barney Potter <barney.potter.24@gmail.com>
2017-06-06 16:41:58 -0700

    Add verbose output

--- pipeline/basecall/metrichor_vs_albacore_setup.py
+++ pipeline/basecall/metrichor_vs_albacore_setup.py
@@ -5,6 +5,7 @@ pass_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/pass/'
 fail_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/fail/'
 new_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/test/'
 
+print('Moving pass files:')
 for barcode in os.listdir(pass_dir):
     bcd = pass_dir + barcode + '/'
     if os.path.isdir(bcd):
@@ -12,10 +13,13 @@ for barcode in os.listdir(pass_dir):
             if f[:-6] == '.fast5':
                 old = bcd + f
                 new = new_dir + f
+                print('mv %s %s'%(old, new))
                 copyfile(old, new)
 
+print('Moving fail files:')
 for f in os.listdir(fail_dir):
     if f[:-6] == '.fast5':
         old = fail_dir + f
         new = new_dir + f
+        print('mv %s %s'%(old, new))
         copyfile(old, new)
