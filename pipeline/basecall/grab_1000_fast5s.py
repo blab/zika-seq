16ad237aee36e88303e7041b5c11f913b310ab84
Barney Potter <barney.potter.24@gmail.com>
2017-06-08 12:46:02 -0700

    Add script to grab files for poretools vs nanopolish comparison

---
+++ pipeline/basecall/grab_1000_fast5s.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+from shutil
+
+from_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/'
+to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/demux/'
+count = 0
+
+for f in os.listdir(from_dir):
+    if f[-6:] == '.fast5' and count < 1000:
+        old = from_dir + f
+        new = to_dir + f
+        print()
+        print('%s: mv %s %s'%(count, old, new))
+        # shutil.copyfile()
+        count += 1
