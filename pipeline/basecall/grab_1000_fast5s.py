06cca6aacac7453b2a54fc1b149a143f2b0794ec
Barney Potter <barney.potter.24@gmail.com>
2017-06-08 13:00:40 -0700

    Take out blocks for poretools test

--- pipeline/basecall/grab_1000_fast5s.py
+++ pipeline/basecall/grab_1000_fast5s.py
@@ -2,16 +2,15 @@
 import os, shutil, sys
 
 from_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/'
-to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/demux/'
+to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/demux/poretools_test/'
 count = 0
 
 for f in os.listdir(from_dir):
     if f[-6:] == '.fast5' and count < 1000:
         old = from_dir + f
         new = to_dir + f
-        print()
         print('%s: mv %s %s'%(count, old, new))
-        # shutil.copyfile()
+        shutil.copyfile(old, new)
         count += 1
     else:
         sys.exit()
