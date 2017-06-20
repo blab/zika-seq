001fddd9072ae276885274cd0c737d35a3ad9238
Barney Potter <barney.potter.24@gmail.com>
2017-06-20 13:37:24 -0700

    Incorporate 1d and 2d scripts together.

--- pipeline/basecall/grab_1000_fast5s.py
+++ pipeline/basecall/grab_1000_fast5s.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
-import os, shutil, sys
+import os, shutil
 
-from_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/'
-to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/demux/poretools_test/'
+parser = argparse.ArgumentParser(description='Move 1000 fast5 files from either: \n\tlibrary1/basecalled_reads/workspace/ to library1/basecalled_reads/workspace/demux/poretools_test\n\tlibrary7/basecalled_reads/workspace/ to library7/basecalled_reads/workspace/demux/poretools_test/')
+parser.add_argument('--dimension', type = str, help="Dimension of reads to grab: 1d or 2d")
+args = parser.parse_args()
+
+if args.dimension == '2d':
+    from_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/'
+    to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library1-2016-12-10/basecalled_reads/workspace/demux/poretools_test/'
+else:
+    from_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library7-1d-2017-03-24/test/workspace/'
+    to_dir = '/fh/fast/bedford_t/zika-seq/data/usvi-library7-1d-2017-03-24/test/workspace/demux/poretools_test/'
+
 count = 0
-
 for f in os.listdir(from_dir):
-    if f[-6:] == '.fast5' and count < 1000:
-        old = from_dir + f
-        new = to_dir + f
-        print('%s: mv %s %s'%(count, old, new))
-        shutil.copyfile(old, new)
-        count += 1
-    else:
-        sys.exit()
+    # Only move 1000 files
+    while count < 1000:
+        if f[-6:] == '.fast5':
+            old = from_dir + f
+            new = to_dir + f
+            print('%s: mv %s %s'%(count, old, new))
+            shutil.copyfile(old, new)
+            count += 1
