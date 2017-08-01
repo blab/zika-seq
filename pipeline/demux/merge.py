f165ace9ff9ecbd6f9fb71e956fb4d3dcacca911
Barney Potter <barney.potter.24@gmail.com>
2017-08-01 13:35:04 -0700

    Execute script.

--- pipeline/demux/merge.py
+++ pipeline/demux/merge.py
@@ -1,7 +1,10 @@
 #!/bin/bash/python
 
 import os, subprocess
+import time
 
+t = time.time()
+count = 0
 cwd = os.getcwd()
 print("cwd: %s" % cwd)
 
@@ -12,4 +15,11 @@ for folder in os.listdir(cwd):
         for f in os.listdir(path):
             call = 'mv %s .' % (path + '/' + f)
             print(call)
-            # subprocess.call(call, shell=True)
+            subprocess.call(call, shell=True)
+            count += 1
+
+        call = 'rmdir %s' % (path)
+        print(call)
+        subprocess.call(call, shell=True)
+
+print("moved %s files in %s seconds" % (count, (time.time()-t)))
