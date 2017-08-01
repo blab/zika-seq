47e3a6552889b6ed5cf34f323013e33b7ac8d0b2
Barney Potter <barney.potter.24@gmail.com>
2017-08-01 13:31:18 -0700

    Bugfixes.

--- pipeline/demux/merge.py
+++ pipeline/demux/merge.py
@@ -6,10 +6,10 @@ cwd = os.getcwd()
 print("cwd: %s" % cwd)
 
 for folder in os.listdir(cwd):
-    path = cwd + folder
+    path = os.path.join(cwd, folder)
     print('~~~~~ path: %s ~~~~~' % (path))
     if os.path.isdir(path):
         for f in os.listdir(path):
-            call = 'mv %s .' % (path + f)
+            call = 'mv %s .' % (path + '/' + f)
             print(call)
             # subprocess.call(call, shell=True)
