60e1fdb3b7826fae3f2bc1a86eadf3be8b3f0748
Barney Potter <barney.potter.24@gmail.com>
2017-09-29 13:25:57 -0700

    Add script to construct small test set from USVI library 8.

---
+++ pipeline/scripts/make_test_set.py
@@ -0,0 +1,14 @@
+import shutil
+import os
+
+workspace = '/fh/fast/bedford_t/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads/workspace'
+new_dir = '/fh/bpotter/test_fast5s'
+count=0
+for n in range(600,610):
+    for f in os.listdir("%s/%s"%(workspace,n)):
+        old = "%s/%s/%s" % (workspace,n,f)
+        new = "%s/%s" % (new_dir,f)
+        shutil.copyfile(old,new)
+        count += 1
+
+print("Moved %s files to %s." % (count,new_dir))
