f7f9daf8b9678bc43d568a0c7e060843812b1bbd
Barney Potter <barney.potter.24@gmail.com>
2017-09-29 15:55:16 -0700

    Add config dictionary for Snakemake runs.

---
+++ cfg.py
@@ -0,0 +1,11 @@
+config = { 'raw_reads':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/0',
+           'dimension':
+                '1d',
+           'demux_dir':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/process/demux',
+           'basecalled_reads':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads',
+           'samples':
+                ['VI39']
+        }
