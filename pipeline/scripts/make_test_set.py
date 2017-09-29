3ee17c95180b72c7b9a0e3826d5b2098ffe40763
Barney Potter <barney.potter.24@gmail.com>
2017-09-29 13:49:02 -0700

    Make output folder in home directory.

--- pipeline/scripts/make_test_set.py
+++ pipeline/scripts/make_test_set.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 workspace = '/fh/fast/bedford_t/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads'
-new_dir = '/fh/bpotter/test_fast5s'
+new_dir = '/home/bpotter/test_fast5s'
 count=0
 for n in range(600,610):
     for f in os.listdir("%s/%s"%(workspace,n)):
