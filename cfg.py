25dfbaf434276143802e05ebfaf1cf24a11ee2a7
Barney Potter <barney.potter.24@gmail.com>
2017-12-11 13:22:14 -0800

    Set cfg.py for all USVI library 8 samples on amphora.

--- cfg.py
+++ cfg.py
@@ -1,5 +1,5 @@
 config = { 'raw_reads':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/0',
+                '/media/amphora/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads',
            'dimension':
                 '1d',
            'demux_dir':
@@ -9,7 +9,7 @@ config = { 'raw_reads':
             'build_dir':
                 '/home/barneypotter/zika-seq/build',
            'samples':
-                ['VI39'],
+                ['VI39', 'VI40', 'VI41', 'VI42', 'VI43'],
            'albacore_config':
                 'r94_450bps_linear.cfg',
            'prefix':
