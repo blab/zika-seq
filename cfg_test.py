33cddbc9ee4dccefb1459990b4f2883f7b5e6f8c
Barney Potter <barney.potter.24@gmail.com>
2017-12-14 10:28:46 -0800

    Update test file paths.

--- cfg_test.py
+++ cfg_test.py
@@ -3,11 +3,11 @@ config = { 'raw_reads':
            'dimension':
                 '1d',
            'demux_dir':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/process/demux',
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/process_test/demux',
            'basecalled_reads':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads',
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads_test',
             'build_dir':
-                '/home/barneypotter/zika-seq/build',
+                '/home/barneypotter/zika-seq/build_test',
            'samples':
                 ['VI39'],
            'albacore_config':
