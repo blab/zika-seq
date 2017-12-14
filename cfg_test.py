dab08dc63400c28cec615dd90b5c0356b2f8e9a7
Barney Potter <barney.potter.24@gmail.com>
2017-12-14 10:44:59 -0800

    Add generic cfg_test

--- cfg_test.py
+++ cfg_test.py
@@ -1,13 +1,13 @@
 config = { 'raw_reads':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/0',
+                '/PATH/TO/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/0',
            'dimension':
                 '1d',
            'demux_dir':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/process_test/demux',
+                '/PATH/TO/zika-seq/data/usvi-library8-1d-2017-03-31/process_test/demux',
            'basecalled_reads':
-                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads_test',
+                '/PATH/TO/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads_test',
             'build_dir':
-                '/home/barneypotter/zika-seq/build_test',
+                '/PATH/TO/zika-seq/build_test',
            'samples':
                 ['VI39'],
            'albacore_config':
---
+++ cfg_test_local.py
@@ -0,0 +1,17 @@
+config = { 'raw_reads':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/raw_reads/0',
+           'dimension':
+                '1d',
+           'demux_dir':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/process_test/demux',
+           'basecalled_reads':
+                '/home/barneypotter/zika-seq/data/usvi-library8-1d-2017-03-31/basecalled_reads_test',
+            'build_dir':
+                '/home/barneypotter/zika-seq/build_test',
+           'samples':
+                ['VI39'],
+           'albacore_config':
+                'r94_450bps_linear.cfg',
+           'prefix':
+                'ZIKA_USVI'
+        }
